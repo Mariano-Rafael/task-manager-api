4d2eb14f9a1e460a5bfffd979820433a35b4a4c6
Rafael Mariano <rmariano38@gmail.com>
2025-10-17 00:37:00 -0300

    feat.: add methods in UserService

--- task-manager-api/src/main/java/io/task_manager/task_manager_api/service/UserService.java
+++ task-manager-api/src/main/java/io/task_manager/task_manager_api/service/UserService.java
@@ -1,6 +1,9 @@
 package io.task_manager.task_manager_api.service;
 
+import io.task_manager.task_manager_api.domain.User;
+import io.task_manager.task_manager_api.dto.UserCreateDTO;
 import io.task_manager.task_manager_api.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +16,27 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    @Transactional
+    public User createUser(UserCreateDTO dto) {
+        userRepository.findByEmail(dto.email()).ifPresent(user -> {
+            throw new IllegalStateException("E-mail já cadastrado.");
+        });
+
+        String hashedPassword = hashPassword(dto.password());
+
+        var newUser = new User(
+                null,
+                dto.name(),
+                dto.email(),
+                hashedPassword
+        );
+        return userRepository.save(newUser);
+    }
+
+    private String hashPassword(String password) {
+        return "hashed_" + password;
+    }
 }
+
+
